b0374334ffdd94a7885e9da78f0e410b912a104d
unknown <anhnt.tb12@gmail.com>
2022-01-05 13:03:49 +0700

    bai tap 3

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ document.write('<br>')
 document.write('b= ' +b)
 document.write('<br>')
 document.write('s= ' +s)
+document.write('<br>')
+let a = 22;
+let c = 5;
+let d = a*c;
+document.write("kết quả là "+ d);
+document.write('<br>')
+
+let x = parseInt(prompt("Nhập số x"));
+let y = parseInt(prompt("Nhập số y"));
+if (x%y == 0 ){
+    alert("x là bội của y");
+}else {
+    alert("x không là bội của y");
+}
+
+
